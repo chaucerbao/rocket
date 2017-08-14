d148a902333246911c9798d1b4efd666461e1129
Alvin Chan <aschan@gmail.com>
2017-08-14 16:42:07 -0700

    Add `parent` selector helper

--- src/lib/selector.ts
+++ src/lib/selector.ts
@@ -3,3 +3,18 @@ export const $ = (target: string, context?: Element) =>
 
 export const $$ = (target: string, context?: Element) =>
   (context || document).querySelectorAll(target)
+
+export const parent = (
+  element: Element,
+  target: string
+): Element | undefined => {
+  const { parentElement } = element
+
+  if (parentElement && parentElement !== document.body) {
+    return parentElement.matches(target)
+      ? parentElement
+      : parent(parentElement, target)
+  }
+
+  return undefined
+}
