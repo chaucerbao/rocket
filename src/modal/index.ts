396da1ad9431d1aa3c7a7d6e680b57d4abefe030
Alvin Chan <aschan@gmail.com>
2017-08-31 15:15:00 -0700

    Disable body scrolling when a Modal is open

--- src/modal/index.ts
+++ src/modal/index.ts
@@ -1,12 +1,14 @@
 import { observable, Observable } from 'riot'
-import { createElement, wrapElement } from '../lib/dom'
+import { $$, createElement, wrapElement } from '../lib/dom'
 
 const modalAttribute = 'data-modal'
 const dialogAttribute = 'data-modal-dialog'
 const openAttribute = 'data-modal-open'
+const freezeAttribute = 'data-modal-freeze'
 
 export default (element: HTMLElement) => {
   const module: Observable = observable()
+  const documentBody = document.body
 
   const modal = createElement('div', [], { [modalAttribute]: '' })
   wrapElement(modal, element)
@@ -15,12 +17,20 @@ export default (element: HTMLElement) => {
   element.addEventListener('load', () => module.trigger('load', element))
 
   const open = () => {
+    if ($$(`[${openAttribute}]`).length === 0)
+      documentBody.setAttribute(freezeAttribute, '')
+
     modal.setAttribute(openAttribute, '')
+
     module.trigger('open', element)
   }
 
   const close = () => {
     modal.removeAttribute(openAttribute)
+
+    if ($$(`[${openAttribute}]`).length === 0)
+      documentBody.removeAttribute(freezeAttribute)
+
     module.trigger('close', element)
   }
 
