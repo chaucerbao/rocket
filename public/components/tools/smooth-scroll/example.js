7f09965be69e746923de2b705670c1583330e6cb
Alvin Chan <aschan@gmail.com>
2017-08-31 15:33:39 -0700

    Update Smooth Scroll example to account for header height

--- public/components/tools/smooth-scroll/example.js
+++ public/components/tools/smooth-scroll/example.js
@@ -7,13 +7,15 @@ import { each } from '../../../../src/lib/utils'
 import './example.css'
 
 const scrollButtons = $$('.scroll-example__button')
+const header = $('.ndpl-header')
 
 each(scrollButtons, button => {
   button.addEventListener('click', () => {
     const scrollHandler = smoothScroll({
       to:
         window.pageYOffset +
-        $('.scroll-example__target').getBoundingClientRect().top
+        $('.scroll-example__target').getBoundingClientRect().top -
+        (window.innerWidth < 960 ? header.getBoundingClientRect().height : 0)
     })
 
     scrollHandler.on('end', () => console.log('End'))
